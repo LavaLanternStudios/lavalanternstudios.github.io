c7eca2f6a93c0b1af885d10dc6f631ee7932852e
LavaLanternStudios <lavalanternstudios@gmail.com>
2026-09-24 21:06:00 +0100

    Update to lava lantern Run devlogs

--- assets/data/devlogs/LavaLanternRun-devlogs.js
+++ assets/data/devlogs/LavaLanternRun-devlogs.js
@@ -5,5 +5,12 @@ window.LavaLanternDevlogs.registerGame("LavaLanternRun-devlogs.js", [
 		image: "assets/images/games/LavaLanternRun_Cover.png",
 		description: "Lava Lantern Run is released and ready to play!",
 		devlogURL: "https://lava-lantern-studios.itch.io/lava-lantern-run/devlog/1668529/lava-lantern-run-full-release"
+	},
+	{
+		date: "2026-09-24",
+		title: "Lava Lantern Run v1.1.0 - Major update to progression and powerups",
+		image: "assets/images/games/LavaLanternRun_Cover.png",
+		description: "The progression system has been fully updated as well as some new power ups!",
+		devlogURL: "https://lava-lantern-studios.itch.io/lava-lantern-run/devlog/1675877/lava-lantern-run-v110"
 	}
 ]);
